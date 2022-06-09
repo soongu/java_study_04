13a58189e66a621ddfd84e22adb6eb9a49820451
soongu <naong526@naver.com>
2022-06-09 12:19:22 +0900

    [22.06.09] static

--- src/datastructure/chap04/투_포인터_02.java
+++ src/datastructure/chap04/투_포인터_02.java
@@ -11,10 +11,51 @@ package datastructure.chap04;
      count를 증가시키고 s++ , e--
 
 */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 백준 1940
 public class 투_포인터_02 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); // 재료의 개수
+        int M = sc.nextInt(); // 갑옷이 완성되는 번호의 합
+        int[] A = new int[N]; // 재료들
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        sc.close();
+
+        // 오름차 정렬
+        Arrays.sort(A);
+//        System.out.println(Arrays.toString(A));
+
+        int count = 0; // 갑옷이 완성되는 경우의 수
+        int start = 0; // 스타트 포인터의 초기 인덱스
+        int end = N - 1; // 엔드 포인터의 초기 인덱스
+
+        // 투 포인터 알고리즘
+        // start와 end가 역전될 때까지
+        while (start < end) {
+            int sum = A[start] + A[end];
+//            System.out.printf("start: %d, end: %d, sum: %d, M: %d\n"
+//                                , start, end, sum, M);
+            if (sum == M) { // 갑옷이 완성되는 경우
+                count++; // 경우의 수 증가
+                start++;
+                end--;
+            } else if (sum > M) {
+                end--;
+            } else {
+                start++;
+            }
+        }
+        System.out.println(count);
 
     }
 }
