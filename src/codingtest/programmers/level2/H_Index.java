e2fe1ee1f80f01353bffdaf0dee878c53238c33a
soongu <naong526@naver.com>
2022-06-14 15:40:02 +0900

    [22.06.13] 인터페이스

--- src/codingtest/programmers/level2/H_Index.java
+++ src/codingtest/programmers/level2/H_Index.java
@@ -6,14 +6,6 @@ public class H_Index {
 
     public static int solution(int[] citations) {
         int answer = 0;
-        // 0, 1, 3, 5, 6
-        Arrays.sort(citations);
-
-        for (int i = 0; i < citations.length; i++) {
-            if (citations[i] >= citations.length - i) {
-                return citations.length - i;
-            }
-        }
 
         return answer;
     }
---
+++ src/com/kh/practice/chap02_abstractNInterface/model/vo/NotePen.java
@@ -0,0 +1,8 @@
+package com.kh.practice.chap02_abstractNInterface.model.vo;
+
+public interface NotePen {
+
+    boolean PEN_BUTTON = true;
+
+    boolean bluetoothPen();
+}
